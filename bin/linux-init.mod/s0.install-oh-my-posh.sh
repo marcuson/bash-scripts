6ee2fb23930dd4c21113c080445c8aaeefe08cb8
Leonardo Marcucci <marcuson.nn90@gmail.com>
2026-06-23 11:57:19 +0200

    fix(linux-init.sh): Use more robust oh my posh installation.

--- bin/linux-init.mod/s0.install-oh-my-posh.sh
+++ bin/linux-init.mod/s0.install-oh-my-posh.sh
@@ -1,38 +1,41 @@
 #!/usr/bin/env bash
 
 LinuxInitMod:installOhMyPosh() {
-    IO:print "Installing oh-my-posh"
+	IO:print "Installing oh-my-posh"
 
-    if ! LinuxInitMod:isNormalUser "$LI__USER"; then
-        IO:die "LI__USER problem, it must be set, it must be a normal user, it must exists"
-    fi
+	if ! LinuxInitMod:isNormalUser "$LI__USER"; then
+		IO:die "LI__USER problem, it must be set, it must be a normal user, it must exists"
+	fi
 
-    sudo -u "$LI__USER" bash -c "curl -s https://ohmyposh.dev/install.sh | bash -s"
+	sudo -u "$LI__USER" bash -c "curl -s https://ohmyposh.dev/install.sh | bash -s"
 
-    if LinuxInitMod:isVarEmpty "$home_user_d"; then
-        home_user_d=$(sudo -u "$LI__USER" sh -c 'echo $HOME')
-    fi
+	if LinuxInitMod:isVarEmpty "$home_user_d"; then
+		home_user_d=$(sudo -u "$LI__USER" sh -c 'echo $HOME')
+	fi
 
-    local profile_user_f="$home_user_d/.profile"
+	local profile_user_f="$home_user_d/.profile"
 
-    if [ ! -f "$profile_user_f" ] || ! grep -q 'oh-my-posh' "$profile_user_f"; then
-        local user_default_shell
-        user_default_shell=$(awk -F: -v user="$LI__USER" '$1 == user {print $NF}' /etc/passwd)
+	if [ ! -f "$profile_user_f" ] || ! grep -q 'oh-my-posh' "$profile_user_f"; then
+		local user_default_shell
+		user_default_shell=$(awk -F: -v user="$LI__USER" '$1 == user {print $NF}' /etc/passwd)
 
-        sudo -u "$LI__USER" "$user_default_shell" -c 'export PATH=$PATH:$HOME/.local/bin; oh-my-posh font install meslo'
+		sudo -u "$LI__USER" "$user_default_shell" -c 'export PATH=$PATH:$HOME/.local/bin; oh-my-posh font install meslo'
 
-        local omp_shell
-        omp_shell=$(sudo -u "$LI__USER" "$user_default_shell" -c 'export PATH=$PATH:$HOME/.local/bin; oh-my-posh get shell; echo $SHELL 2>&1 > /dev/null')
+		local omp_shell
+		omp_shell=$(sudo -u "$LI__USER" "$user_default_shell" -c 'export PATH=$PATH:$HOME/.local/bin; oh-my-posh get shell; echo $SHELL 2>&1 > /dev/null')
 
-        sudo -u "$LI__USER" bash -c "wget https://raw.githubusercontent.com/Nick2bad4u/OhMyPosh-Atomic-Enhanced/main/OhMyPosh-Atomic-Custom-ExperimentalDividers.json -O $home_user_d/.omp.json"
+		sudo -u "$LI__USER" bash -c "wget https://raw.githubusercontent.com/Nick2bad4u/OhMyPosh-Atomic-Enhanced/main/OhMyPosh-Atomic-Custom-ExperimentalDividers.json -O $home_user_d/.omp.json"
 
-        # shellcheck disable=SC2086
-        echo -e "" | tee -a "$profile_user_f" >/dev/null
-        echo -e "eval \"\$(oh-my-posh init $omp_shell --config $home_user_d/.omp.json)\"" | tee -a "$profile_user_f" >/dev/null
-        IO:print "oh-my-posh installed"
-    else
-        IO:print "$profile_user_f already configured"
-    fi
+		cat <<EOF >>"$profile_user_f"
+if [ -n "\$DISPLAY" ] || [ -n "\$WAYLAND_DISPLAY" ] || [ "\$TERM" = "xterm-256color" ]; then
+    eval \"\$(oh-my-posh init $omp_shell --config $home_user_d/.omp.json)\""
+fi
+EOF
 
-    return 0
+		IO:print "oh-my-posh installed"
+	else
+		IO:print "$profile_user_f already configured"
+	fi
+
+	return 0
 }
